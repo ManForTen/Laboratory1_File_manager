8d151f5d8d9b369113d9df34404176347fd92288
Artem Petrikov <manforten@tandex.ru>
2022-10-02 17:18:05 +0300

    New functions.py and update filemanager.py with new console menu

--- filemanager.py
+++ filemanager.py
@@ -1,48 +1,24 @@
-import os # Для создания папки
-import shutil # Для копирования файлов и папок
+from functions import create_file, delete_file, copy_file, create_folder, get_list
 
-def create_file(name, text=None):
-    with open(name, 'w', encoding = 'utf-8') as f:
-        if text:
-            f.write(text)
+print('''Действия:
+---------------------------------------------------------------------------------------------------------
+1. Создание папки (с указанием имени);
+2. Удаление папки по имени;
+3. Перемещение между папками (в пределах рабочей папки) - заход в папку по имени, выход на уровень вверх;
+4. Создание пустых файлов с указанием имени;
+5. Запись текста в файл;
+6. Просмотр содержимого текстового файла;
+7. Удаление файлов по имени;
+8. Копирование файлов из одной папки в другую;
+9. Перемещение файлов;
+10. Переименование файлов.
+---------------------------------------------------------------------------------------------------------
+''')
 
-def delete_file(name):
-    if os.path.isdir(name): # Если это папка удаляем её
-        os.rmdir(name)
-    else: # Иначе удаляем файл
-        os.remove(name)
+A = int(input('Введите цифру:'))
 
-def copy_file(name, new_name):
-    if os.path.isdir(name): # Если это папка, то копируем её
-        try:
-            shutil.copytree(name, new_name)
-        except FileExistsError:
-            print('Даная папка уже существует!')
-    else: # Иначе копируем файл
-        shutil.copy(name, new_name)
+if A == 1:
+    N = input('Введите название папки:')
+    create_folder(N)
+    print('Папки, которые сейчас находятся в этом пути:',get_list(True))
 
-
-def create_folder(name):
-    try:
-        os.mkdir(name)
-    except FileExistsError:
-        print('Даная папка уже существует!')
-
-def get_list(folders_only = False):
-    result = os.listdir()
-    if folders_only:
-        result = [f for f in result if os.path.isdir(f)]
-    print(result)
-
-if __name__ == '__main__':
-    create_file('test1.txt')
-    create_file('test2.txt', 'Hello world!')
-    create_folder('new_f')
-    create_folder('new_f1')
-    get_list()
-    get_list(True) # Только папки
-    delete_file('new_f')
-    delete_file('test1.txt')
-    copy_file('new_f1','new_f2')
-    create_file('test3.txt','How are you?')
-    copy_file('test3.txt', 'test4.txt')
---
+++ functions.py
@@ -0,0 +1,48 @@
+import os # Для создания папки
+import shutil # Для копирования файлов и папок
+
+def create_file(name, text=None):
+    with open(name, 'w', encoding = 'utf-8') as f:
+        if text:
+            f.write(text)
+
+def delete_file(name):
+    if os.path.isdir(name): # Если это папка удаляем её
+        os.rmdir(name)
+    else: # Иначе удаляем файл
+        os.remove(name)
+
+def copy_file(name, new_name):
+    if os.path.isdir(name): # Если это папка, то копируем её
+        try:
+            shutil.copytree(name, new_name)
+        except FileExistsError:
+            print('Даная папка уже существует!')
+    else: # Иначе копируем файл
+        shutil.copy(name, new_name)
+
+
+def create_folder(name):
+    try:
+        os.mkdir(name)
+    except FileExistsError:
+        print('Даная папка уже существует!')
+
+def get_list(folders_only = False):
+    result = os.listdir()
+    if folders_only:
+        result = [f for f in result if os.path.isdir(f)]
+    print(result)
+
+if __name__ == '__main__':
+    create_file('test1.txt')
+    create_file('test2.txt', 'Hello world!')
+    create_folder('new_f')
+    create_folder('new_f1')
+    get_list()
+    get_list(True) # Только папки
+    delete_file('new_f')
+    delete_file('test1.txt')
+    copy_file('new_f1','new_f2')
+    create_file('test3.txt')
+    copy_file('test3.txt', 'test4.txt')
